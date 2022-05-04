627dfb7789719e0541f34a8008cb7c70d3ccf61b
Sparsh <sparshbatta2000@gmail.com>
2022-05-05 01:31:45 +0530

    secon211

--- base/settings.py
+++ base/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d7cv4hlkucezjwvr=&c!f4-myzuz&6v)wgmi#y#hchg9cq0xh@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- base/wsgi.py
+++ base/wsgi.py
@@ -10,13 +10,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-
-from base import MyWSGIApp
-
-application = MyWSGIApp()
-application = WhiteNoise(application, root="/path/to/static/files")
-application.add_files("/path/to/more/static/files", prefix="more-files/")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 
