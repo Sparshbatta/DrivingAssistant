dec02550d457a0bd3a0fb7d1db25849b0feaaf0a
Sparsh <sparshbatta2000@gmail.com>
2022-05-05 01:26:20 +0530

    secons1

--- base/settings.py
+++ base/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'accounts',
 ]
 
