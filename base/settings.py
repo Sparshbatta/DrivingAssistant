ede150ddcb4f7724aeb35925a014833af0deedf1
Sparsh <sparshbatta2000@gmail.com>
2022-05-05 01:53:44 +0530

    third

--- base/settings.py
+++ base/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-d7cv4hlkucezjwvr=&c!f4-myzuz&6v)wgmi#y#hchg9cq0xh@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
